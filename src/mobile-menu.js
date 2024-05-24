6fe7113a6295a1a813997fc3351a81fad7ac4908
Oleh Mogylevsky <159718895+mogilevs@users.noreply.github.com>
2024-05-24 17:58:44 +0300

    add mob menu close after choosing

--- src/mobile-menu.js
+++ src/mobile-menu.js
@@ -2,18 +2,19 @@
   const mobileMenu = document.querySelector('.js-menu-container');
   const openMenuBtn = document.querySelector('.js-open-menu');
   const closeMenuBtn = document.querySelector('.js-close-menu');
-
+  const closeMenu = document.querySelectorAll('.mob-menu-link');
+  
   const toggleMenu = () => {
     const isMenuOpen =
       openMenuBtn.getAttribute('aria-expanded') === 'true' || false;
     openMenuBtn.setAttribute('aria-expanded', !isMenuOpen);
     mobileMenu.classList.toggle('is-open');
-
-    
   };
 
   openMenuBtn.addEventListener('click', toggleMenu);
   closeMenuBtn.addEventListener('click', toggleMenu);
+  closeMenu.forEach(element => element.addEventListener('click', toggleMenu));
+  
 
   // Close the mobile menu on wider screens if the device orientation changes
   window.matchMedia('(min-width: 768px)').addEventListener('change', e => {

b85c6433bcb130697173e7b06d18a6de6d292124
Oleh Mogylevsky <159718895+mogilevs@users.noreply.github.com>
2024-06-21 10:58:09 +0300

    add footer links scroll

--- src/main.js
+++ src/main.js
@@ -1,5 +1,5 @@
-import "./mobile-menu.js"
-import scroll from './smoothScroll.js'
+import './mobile-menu.js';
+import scroll from './smoothScroll.js';
 
-const menu = document.querySelectorAll('.menu-link');
+const menu = document.querySelectorAll('.menu-link, .js-item');
 menu.forEach(element => element.addEventListener('click', scroll));
